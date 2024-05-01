e84b112a5a9ecb32d6b0c25ef6fc833749bcdb15
rick-sys <ricksanchezy2x@gmail.com>
2024-05-01 21:03:38 +0200

    tttttt

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-
+import { redirect } from "next/navigation";
 
 import { UtopiaHero } from "@/components/utopia-hero";
 
